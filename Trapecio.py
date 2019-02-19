e6cfe94734083efc42926dbbe15ea0620006f0d1
Iván <46821602+VictorIvanMr@users.noreply.github.com>
2019-02-18 19:11:11 -0600

    Update Trapecio.py

--- Trapecio.py
+++ Trapecio.py
@@ -1,16 +1,19 @@
 #Víctor Iván Morales Ramos A01377601
 #Descripción: Calcular el perimetro y el area de un trapecio
 
+#Funcion que calcula el area
 def calcularArea(bmayor, bmenor, h):
     a = ((bmayor + bmenor)/2)*h
     return a
 
+#Función que calcula el perimetro
 def calcularPerimetro(bmayor, bmenor, h):
     bl = (bmayor - bmenor)/2
     l = (bl **2 + h**2) ** 0.5
     p = bmayor + bmenor + (2 * l)
     return p
 
+#Llamada de la función
 def main():
     bmayor = int(input("Dame el tamaño de la base mayor: "))
     bmenor = int(input("Dame el tamaño de la base menor: "))
